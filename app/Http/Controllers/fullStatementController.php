fb2ce9df0eb37bda4f567d6e21834d666675fb53
Mutombo <mgtshomba@gmail.com>
2021-01-11 22:04:39 +0200

    Query database to return all transactions

--- app/Http/Controllers/fullStatementController.php
+++ app/Http/Controllers/fullStatementController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transactions;
 
 class fullStatementController extends Controller
 {
     public function index()
     {
-        return view('fullStatement');
+        $allTransaction = Transactions::all();
+        return view('fullStatement')->with('allTransaction', $allTransaction);
     }
 }
--- resources/views/fullStatement.blade.php
+++ resources/views/fullStatement.blade.php
@@ -1,5 +1,38 @@
 @extends('layouts.layout')
 @section('content')
-<h4>Complete statement</h4>            
-@endsection
+<h3> All Transactions </h3>
+{{-- {{ $allTransaction }} --}}
 
+<table>
+    <thead>
+        <thead>
+            <th>ID</th>
+            <th>Transaction name</th>
+            <th>Transaction Amount</th>
+            <th>Transaction type</th>
+            <th>Transaction Description</th>
+            <th>Transaction Date</th>
+        </thead>
+        <tbody>
+            @foreach ($allTransaction as $transaction )
+
+            <tr><td>{{ $transaction->id }}</td>
+                <td>{{ $transaction->transaction_name }}</td>
+                
+                <td>@if ($transaction->transaction_type_id == 1)
+                 R {{ $transaction->transaction_amount }}
+                @else 
+                - R {{ $transaction->transaction_amount }}
+                @endif</td>
+                <td>{{ $transaction->transaction_name }}</td>
+                <td>{{ $transaction->transaction_description}}</td>
+                <td>{{ $transaction->transaction_date}}</td>
+            </tr>
+    
+@endforeach
+            
+        </tbody>
+    </thead>
+</table>
+<a href="/home"> Home</a>
+@endsection
